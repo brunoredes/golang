0bc20b1c56f08fb387e95c234a4282f077249220
Bruno Donatelli <bruno.donatelli17@gmail.com>
2024-10-15 14:09:47 -0300

    feat: add scanner
    Intro sobre ponteiros e scanner

---
+++ alura/observador.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	nome := "Bruno"
+	// idade := 27
+	versao := 1.1
+	fmt.Println("Olá sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+
+	var comando int
+
+	fmt.Scan(&comando)
+}
